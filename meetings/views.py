7de1da47cf529cdc17a36fb1e7789ade3e11d64e
liuqi <469227928@qq.com>
2021-01-25 10:50:27 +0800

    fix a bug that may lead to an earlier day

--- meetings/views.py
+++ meetings/views.py
@@ -461,8 +461,8 @@ class MeetingsView(GenericAPIView, CreateModelMixin):
             start_time = date + 'T' + ':'.join([str(int(start.split(':')[0]) - 8), start.split(':')[1], '00Z'])
         else:
             d = datetime.datetime.strptime(date, '%Y-%m-%d') - datetime.timedelta(days=1)
-            date = datetime.datetime.strftime(d, '%Y-%m-%d %H%M%S')[:10]
-            start_time = date + 'T' + ':'.join([str(int(start.split(':')[0]) + 16), start.split(':')[1], '00Z'])
+            d2 = datetime.datetime.strftime(d, '%Y-%m-%d %H%M%S')[:10]
+            start_time = d2 + 'T' + ':'.join([str(int(start.split(':')[0]) + 16), start.split(':')[1], '00Z'])
         # 计算duration
         duration = (int(end.split(':')[0]) - int(start.split(':')[0])) * 60 + (
                 int(end.split(':')[1]) - int(start.split(':')[1]))
