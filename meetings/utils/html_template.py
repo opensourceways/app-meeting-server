d11287679e986dc4cafccd3593e5f9073878cd4b
liuqi <469227928@qq.com>
2021-03-01 16:06:57 +0800

    change the cover and enrich the video title of bilibili

--- meetings/utils/html_template.py
+++ meetings/utils/html_template.py
@@ -6,13 +6,12 @@ def cover_content(topic, group_name, date, start_time, end_time):
         <meta charset="UTF-8">
         <title>cover</title>
     </head>
-    <body style="text-align: center">
-    <div style="display: inline-block; width: 63ream; height: 42rem">
-        <img src="cover.png" alt="recording cover" style="margin-top: 5rem"/>
-        <h1 style="color: #002FA7; margin-top: 3rem">{0}</h1>
-        <h4>SIG: {1}</h4>
-        <h4>Time: {2} {3}-{4}</h4>
-    </div>
+    <body>
+        <div style="display: inline-block; height: 688px; width: 1024px; text-align: center; background-image: url('cover.png')">
+            <p style="font-size: 100px;margin-top: 150px; color: white"><b>{0}</b></p>
+            <p style="font-size: 80px; margin: 0; color: white">SIG: {1}</p>
+            <p style="font-size: 60px; margin: 0; color: white">Time: {2} {3}-{4}</p>
+        </div>
     </body>
     </html>
     """.format(topic, group_name, date, start_time, end_time)
